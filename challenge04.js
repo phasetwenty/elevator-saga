89bb74681a63972bf31b2534bb988bfc89990ca5
Christopher Haverman <chaverma@alumni.uci.edu>
2017-02-19 22:25:30 -0800

    Challenge 4: added a function which will add floor requests to a global queue.

--- challenge04.js
+++ challenge04.js
@@ -1,16 +1,37 @@
 {
   init: function(elevators, floors) {
-    elevators.forEach(function(elevator, index, arr) {
-      var i = 0;
-      elevator.on("idle", function() {
-        elevator.goToFloor(i++);
-        if (i > floors.length) {
-          i = 0;
-        }
+    window._allDestinations = [];
+
+    var self = this;
+    floors.forEach(function(floor, index) {
+      floor.on('up_button_pressed', function() {
+        self._floor_button_pressed(floor);
+        console.log('Global request queue is: ' + window._allDestinations.toString());
+      });
+      floor.on('down_button_pressed', function() {
+        self._floor_button_pressed(floor);
+        console.log('Global request queue is: ' + window._allDestinations.toString());
       });
     });
-  }
+    // elevators.forEach(function(elevator, index, arr) {
+    //   elevator.on('idle', function() {
+    //     floors.forEach(function (floor) {
+    //       elevator.goToFloor(floor.floorNum());
+    //     });
+    //   });
+    // });
+  },
   update: function(dt, elevators, floors) {
     // We normally don't need to do anything here
+  },
+
+  _floor_button_pressed: function(floor) {
+    var floorNum = floor.floorNum();
+    if (window._allDestinations.indexOf(floorNum) === -1) {
+      window._allDestinations.push(floorNum);
+      console.log('Added ' + floorNum + ' to requested floors.');
+    } else {
+      console.log('Received a request for ' + floorNum + ' but it\'s already in the queue.');
+    }
   }
 }
