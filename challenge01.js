2f8d0e4ec2bc1f28b212955b8a2fdc3e723ae3ce
Christopher Haverman <chaverma@alumni.uci.edu>
2017-02-19 21:48:25 -0800

    * Discovered some buggy behavior in exercise 1-3 * .gitignore

--- challenge01.js
+++ challenge01.js
@@ -1,15 +1,14 @@
 {
-  init: function(elevators, floors) {
-    var elevator = elevators[0];
-    var i = 0;
-    elevator.on("idle", function() {
-        elevator.goToFloor(i++);
-        if (i > floors.length) {
-          i = 0;
-        }
-    });
-  },
-  update: function(dt, elevators, floors) {
-    // We normally don't need to do anything here
-  }
+    init: function (elevators, floors) {
+        var elevator = elevators[0];
+        elevator.on("idle", function () {
+            floors.forEach(function (floor) {
+                elevator.goToFloor(floor.floorNum());
+            });
+        });
+    },
+
+    update: function (dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
 }
