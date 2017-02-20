b840d203a00fa4e4e497fdd1da6ae008a83b797c
Christopher Haverman <chaverma@alumni.uci.edu>
2017-02-19 23:00:57 -0800

    Challenge 4: made a function that enables elevators to pull locations from the global request queue.

--- challenge04.js
+++ challenge04.js
@@ -1,16 +1,24 @@
 {
   init: function(elevators, floors) {
-    window._allDestinations = [];
+    var allDestinations = [];
 
     var self = this;
-    floors.forEach(function(floor, index) {
+    elevators.forEach(function(elevator, index) {
+      elevator._id = index + 1;
+      elevator.on('idle', function() {
+        self._elevator_idle(elevator, allDestinations);
+        console.log('Requested floors: ' + elevator.getPressedFloors());
+      });
+    });
+
+    floors.forEach(function(floor) {
       floor.on('up_button_pressed', function() {
-        self._floor_button_pressed(floor);
-        console.log('Global request queue is: ' + window._allDestinations.toString());
+        self._floor_button_pressed(floor, allDestinations);
+        console.log('Global request queue is: ' + allDestinations.toString());
       });
       floor.on('down_button_pressed', function() {
-        self._floor_button_pressed(floor);
-        console.log('Global request queue is: ' + window._allDestinations.toString());
+        self._floor_button_pressed(floor, allDestinations);
+        console.log('Global request queue is: ' + allDestinations.toString());
       });
     });
     // elevators.forEach(function(elevator, index, arr) {
@@ -25,10 +33,19 @@
     // We normally don't need to do anything here
   },
 
-  _floor_button_pressed: function(floor) {
+  _elevator_idle: function(elevator, allDestinations) {
+    var floorNum = allDestinations.shift();
+    if (floorNum === undefined) {
+      return;
+    }
+    console.log('Sending elevator to floor ' + floorNum);
+    elevator.goToFloor(floorNum);
+  },
+
+  _floor_button_pressed: function(floor, allDestinations) {
     var floorNum = floor.floorNum();
-    if (window._allDestinations.indexOf(floorNum) === -1) {
-      window._allDestinations.push(floorNum);
+    if (allDestinations.indexOf(floorNum) === -1) {
+      allDestinations.push(floorNum);
       console.log('Added ' + floorNum + ' to requested floors.');
     } else {
       console.log('Received a request for ' + floorNum + ' but it\'s already in the queue.');
